75b5da81a7ada8d7e5dee4b6e660b30c6da1c8da
tanmoydutta114 <tanmoydutta114@outlook.com>
2024-01-30 23:19:42 +0530

    Fixing Database connection for deployment.

--- cloud-run/src/sql/database.ts
+++ cloud-run/src/sql/database.ts
@@ -2,17 +2,31 @@ import { DB } from "../../kysely/db";
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 
+// TODO: Make the function as dialect only call once per connection
+
+let poolInstance: Pool;
+
+const createPool = () => {
+  return new Pool({
+    database: process.env.DB_NAME,
+    host: process.env.CLOUD_SQL_CONNECTION_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    max: 40,
+    connectionTimeoutMillis: 3000,
+    idleTimeoutMillis: 30000,
+  });
+};
+
+const getPoolInstance = () => {
+  if (!poolInstance) {
+    poolInstance = createPool();
+  }
+  return poolInstance;
+};
 
 const dialect = new PostgresDialect({
-  pool: async () =>
-    new Pool({
-      database: process.env.DB_NAME,
-      host: process.env.CLOUD_SQL_CONNECTION_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      max: 10,
-      connectionTimeoutMillis: 3000,
-    }),
+  pool: async () => getPoolInstance(),
 });
 
 export function getSQLClient() {
